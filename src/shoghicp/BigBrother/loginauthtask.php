1c05b121bc4f75bcdff3da5898f2923860f1f904
lovetwice1012 <59718878+lovetwice1012@users.noreply.github.com>
2021-04-10 10:25:17 +0900

    Update loginauthtask.php

--- src/shoghicp/BigBrother/loginauthtask.php
+++ src/shoghicp/BigBrother/loginauthtask.php
@@ -1,6 +1,51 @@
 <?php
+declare(strict_types=1);
 
-    loginauthtask extends AsyncTask{
+namespace shoghicp\BigBrother;
+
+use pocketmine\network\mcpe\protocol\types\GeneratorType;
+use pocketmine\network\mcpe\VerifyLoginTask;
+use pocketmine\Player;
+use pocketmine\Server;
+use pocketmine\event\server\DataPacketReceiveEvent;
+use pocketmine\item\Item;
+use pocketmine\inventory\CraftingGrid;
+use pocketmine\math\Vector3;
+use pocketmine\network\mcpe\protocol\types\WindowTypes;
+use pocketmine\network\mcpe\protocol\ContainerOpenPacket;
+use pocketmine\network\mcpe\protocol\ProtocolInfo as Info;
+use pocketmine\network\mcpe\protocol\LoginPacket;
+use pocketmine\network\mcpe\protocol\RequestChunkRadiusPacket;
+use pocketmine\network\mcpe\protocol\ResourcePackClientResponsePacket;
+use pocketmine\network\mcpe\protocol\DataPacket;
+use pocketmine\network\mcpe\protocol\BatchPacket;
+use pocketmine\network\mcpe\protocol\SetLocalPlayerAsInitializedPacket;
+use pocketmine\network\SourceInterface;
+use pocketmine\scheduler\AsyncTask;
+use pocketmine\level\Level;
+use pocketmine\level\format\Chunk;
+use pocketmine\timings\Timings;
+use pocketmine\utils\Internet;
+use pocketmine\utils\TextFormat;
+use pocketmine\utils\UUID;
+use shoghicp\BigBrother\network\Packet;
+use shoghicp\BigBrother\network\protocol\Login\EncryptionRequestPacket;
+use shoghicp\BigBrother\network\protocol\Login\EncryptionResponsePacket;
+use shoghicp\BigBrother\network\protocol\Login\LoginSuccessPacket;
+use shoghicp\BigBrother\network\protocol\Play\Server\AdvancementsPacket;
+use shoghicp\BigBrother\network\protocol\Play\Server\KeepAlivePacket;
+use shoghicp\BigBrother\network\protocol\Play\Server\PlayerPositionAndLookPacket;
+use shoghicp\BigBrother\network\protocol\Play\Server\TitlePacket;
+use shoghicp\BigBrother\network\protocol\Play\Server\SelectAdvancementTabPacket;
+use shoghicp\BigBrother\network\protocol\Play\Server\UnloadChunkPacket;
+use shoghicp\BigBrother\network\ProtocolInterface;
+use shoghicp\BigBrother\entity\ItemFrameBlockEntity;
+use shoghicp\BigBrother\utils\Binary;
+use shoghicp\BigBrother\utils\InventoryUtils;
+use shoghicp\BigBrother\utils\RecipeUtils;
+use shoghicp\BigBrother\utils\SkinImage;
+
+    class loginauthtask extends AsyncTask{
 
 				/** @var string */
 				private $username;
