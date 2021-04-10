25aa5cac2e9c62b0a91d5f7f43e3d4471882d113
lovetwice1012 <59718878+lovetwice1012@users.noreply.github.com>
2021-04-10 10:41:17 +0900

    Update ConvertUtils.php

--- src/shoghicp/BigBrother/utils/ConvertUtils.php
+++ src/shoghicp/BigBrother/utils/ConvertUtils.php
@@ -789,19 +789,4 @@ class ConvertUtils{
 	}
 
 }
-
-
-class ComputerItem extends Item{
-	/**
-	 * @param int                $id
-	 * @param int                $meta
-	 * @param int                $count
-	 * @param CompoundTag|string $tag
-	 */
-	public function __construct(int $id = 0, int $meta = 0, int $count = 1, $tag = ""){
-		parent::__construct($id, $meta);
-		$this->setCount($count);
-		$this->setCompoundTag($tag);
-	}
-
 }
