add9ec7b736203892dae2b663388200dc92b0c67
lovetwice1012 <59718878+lovetwice1012@users.noreply.github.com>
2021-04-10 10:33:21 +0900

    Create handleauthtask.php

---
+++ src/shoghicp/BigBrother/handleauthtask.php
@@ -0,0 +1,12 @@
+<?php
+declare(strict_types=1);
+
+namespace shoghicp\BigBrother;
+
+use pocketmine\Player;
+use pocketmine\Server;
+use pocketmine\scheduler\AsyncTask;
+use pocketmine\level\Level;
+use pocketmine\utils\Internet;
+
+    class handleauthtask 
