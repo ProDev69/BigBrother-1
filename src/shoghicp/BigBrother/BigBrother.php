46e03bba76b0c0c2bbb56657f6fdbe3f49ef577b
ProDev69 <87289137+ProDev69@users.noreply.github.com>
2021-07-12 07:44:15 +0530

    1.17 update

--- src/shoghicp/BigBrother/BigBrother.php
+++ src/shoghicp/BigBrother/BigBrother.php
@@ -95,7 +95,7 @@ class BigBrother extends PluginBase implements Listener{
 		}
 
 		if($enable){
-			if(Info::CURRENT_PROTOCOL === 431){
+			if(Info::CURRENT_PROTOCOL === 440){
 				ConvertUtils::init();
 
 				$this->saveDefaultConfig();
